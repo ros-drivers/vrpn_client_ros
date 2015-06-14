0f0927d36a422703b317887c237c515108511cf0
Paul Bovbel <pbovbel@clearpathrobotics.com>
2015-06-14 18:42:37 -0400

    Initial prototype

--- src/vrpn_ros.cpp
+++ src/vrpn_ros.cpp
@@ -1,115 +1,316 @@
-//
-// Created by pbovbel on 12/06/15.
-//
-
 #include "vrpn_ros/vrpn_ros.h"
-
 #include <vrpn_Tracker.h>
 #include <vrpn_Connection.h>
 #include <vrpn_Types.h>
 #include "ros/ros.h"
 #include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/TwistStamped.h>
+#include <geometry_msgs/AccelStamped.h>
+#include <geometry_msgs/TransformStamped.h>
+#include <tf2/LinearMath/Quaternion.h>
+#include <tf2/LinearMath/Matrix3x3.h>
+#include <tf2_ros/transform_broadcaster.h>
 
-ros::Time last_time;
-
-int position_m_id, velocity_m_id, accel_m_id;
-std::string frame_id;
-
-
-//
-//void VRPN_CALLBACK handle_tracker(void* userData, const vrpn_TRACKERCB t )
-//{
-//
-//  ros::Time this_time = ros::Time::now();
-//
-//  ros::Duration elapsed = this_time - last_time;
-//  last_time = this_time;
-//
-////  ROS_INFO_STREAM("Tracker " << t.sensor << "' : " << t.pos[0] << "," <<  t.pos[1] << "," << t.pos[2]);
-//  ROS_INFO_STREAM("F " << 1/elapsed.toSec());
-//
-//}
-//
-//void VRPN_CALLBACK handle_tracker_vel(void* userData, const vrpn_TRACKERVELCB t )
-//{
-//  std::cout << "Tracker '" << t.sensor << "' : " << t.vel[0] << "," <<  t.vel[1] << "," << t.vel[2] << std::endl;
-//}
-
-typedef boost::shared_ptr<vrpn_Connection> ConnectionPtr;
-//typedef boost::shared_ptr<vrpn_Tracker_Remote> TrackerPtr;
-
-boost::shared_ptr<vrpn_Connection> connection_;
-//std::vector<TrackerPtr> trackers_;
-
-struct TrackerRemoteExposer : vrpn_Tracker_Remote
-{
-  using vrpn_Tracker_Remote::handle_change_message;
-  using vrpn_Tracker_Remote::handle_vel_change_message;
-  using vrpn_Tracker_Remote::handle_acc_change_message;
-};
-
-struct ConnectionExposer : vrpn_Connection
-{
-  using vrpn_Connection::message_type_is_registered;
-};
-
-int handle_all(void *userdata, vrpn_HANDLERPARAM message)
+namespace vrpn_ros
 {
 
-  position_m_id = ((connection_.get())->*(&ConnectionExposer::message_type_is_registered))(
-      "vrpn_Tracker Pos_Quat");
-  velocity_m_id = ((connection_.get())->*(&ConnectionExposer::message_type_is_registered))(
-      "vrpn_Tracker Velocity");
-  accel_m_id = ((connection_.get())->*(&ConnectionExposer::message_type_is_registered))(
-      "vrpn_Tracker Acceleration");
+  typedef boost::shared_ptr<vrpn_Connection> ConnectionPtr;
+  typedef boost::shared_ptr<vrpn_Tracker_Remote> TrackerRemotePtr;
 
-  ROS_INFO_STREAM("Got " << connection_->message_type_name(message.type) << " from " <<
-                  connection_->sender_name(message.sender));
-
-  std_msgs::Header header;
-  header.frame_id = frame_id;
-
-//  if(true/*TODO*/){
-//  header.stamp = ros::Time(message.msg_time.tv_sec, message.msg_time.tv_usec*1000);
-//  }else{
-    header.stamp = ros::Time::now();
-//  }
-
-  ROS_INFO_STREAM(message.type << " " << position_m_id << " " << velocity_m_id);
-
-  if (message.type == position_m_id)
-  {
-    geometry_msgs::PoseStamped pose;
-    vrpn_int32 sensor, padding;
-
-    vrpn_unbuffer(&message.buffer, &sensor);
-    vrpn_unbuffer(&message.buffer, &padding);
-
-    vrpn_unbuffer(&message.buffer, &pose.pose.position.x);
-    vrpn_unbuffer(&message.buffer, &pose.pose.position.y);
-    vrpn_unbuffer(&message.buffer, &pose.pose.position.z);
-    vrpn_unbuffer(&message.buffer, &pose.pose.orientation.w);
-    vrpn_unbuffer(&message.buffer, &pose.pose.orientation.x);
-    vrpn_unbuffer(&message.buffer, &pose.pose.orientation.y);
-    vrpn_unbuffer(&message.buffer, &pose.pose.orientation.z);
-
-
-    pose.header = header;
-    ROS_INFO_STREAM(pose);
-
-  }
-  else if (message.type == velocity_m_id)
+  class VrpnTrackerRos
   {
 
-  }
-  else if (message.type == accel_m_id)
+  public:
+
+    VrpnTrackerRos(std::string tracker_name, ConnectionPtr connection, ros::NodeHandle nh, std::string frame_id,
+                   bool use_server_time, bool broadcast_tf)
+        : nh_(nh, tracker_name), use_server_time_(use_server_time), broadcast_tf_(broadcast_tf)
+    {
+//      nh_ = ros::NodeHandle(nh, tracker_name);
+//      frame_id_ = frame_id;
+//      use_server_time_ = use_server_time;
+//      broadcast_tf_ = broadcast_tf;
+
+      pose_msg.header.frame_id = twist_msg.header.frame_id = accel_msg.header.frame_id = transform_stamped.header.frame_id = frame_id;
+      transform_stamped.child_frame_id = tracker_name;
+
+      tracker_remote_ = boost::make_shared<vrpn_Tracker_Remote>(tracker_name.c_str(), connection.get());
+      tracker_remote_->register_change_handler(this, &VrpnTrackerRos::handle_pose);
+      tracker_remote_->register_change_handler(this, &VrpnTrackerRos::handle_twist);
+      tracker_remote_->register_change_handler(this, &VrpnTrackerRos::handle_accel);
+    }
+
+    ~VrpnTrackerRos()
+    {
+      tracker_remote_->unregister_change_handler(this, &VrpnTrackerRos::handle_pose);
+      tracker_remote_->unregister_change_handler(this, &VrpnTrackerRos::handle_twist);
+      tracker_remote_->unregister_change_handler(this, &VrpnTrackerRos::handle_accel);
+    }
+
+    void mainloop()
+    {
+      tracker_remote_->mainloop();
+    }
+
+  private:
+
+    TrackerRemotePtr tracker_remote_;
+    ros::Publisher pose_pub, twist_pub, accel_pub;
+    ros::NodeHandle nh_;
+    bool use_server_time_, broadcast_tf_;
+
+    geometry_msgs::PoseStamped pose_msg;
+    geometry_msgs::TwistStamped twist_msg;
+    geometry_msgs::AccelStamped accel_msg;
+    geometry_msgs::TransformStamped transform_stamped;
+
+    static void VRPN_CALLBACK handle_pose(void *userData, const vrpn_TRACKERCB tracker_pose)
+    {
+
+      VrpnTrackerRos *tracker = (VrpnTrackerRos *) userData;
+      if (tracker->pose_pub.getTopic().empty())
+      {
+        tracker->pose_pub = tracker->nh_.advertise<geometry_msgs::PoseStamped>("pose", 1);
+      }
+
+      if (tracker->pose_pub.getNumSubscribers() > 0)
+      {
+        if (tracker->use_server_time_)
+        {
+          tracker->pose_msg.header.stamp.sec = tracker_pose.msg_time.tv_sec;
+          tracker->pose_msg.header.stamp.nsec = tracker_pose.msg_time.tv_usec * 1000;
+        }
+        else
+        {
+          tracker->pose_msg.header.stamp = ros::Time::now();
+        }
+
+        tracker->pose_msg.pose.position.x = tracker_pose.pos[0];
+        tracker->pose_msg.pose.position.y = tracker_pose.pos[1];
+        tracker->pose_msg.pose.position.z = tracker_pose.pos[2];
+
+        tracker->pose_msg.pose.orientation.x = tracker_pose.quat[0];
+        tracker->pose_msg.pose.orientation.y = tracker_pose.quat[1];
+        tracker->pose_msg.pose.orientation.z = tracker_pose.quat[2];
+        tracker->pose_msg.pose.orientation.w = tracker_pose.quat[3];
+
+        tracker->pose_pub.publish(tracker->pose_msg);
+      }
+
+      if (tracker->broadcast_tf_)
+      {
+
+        static tf2_ros::TransformBroadcaster tf_broadcaster;
+        tracker->transform_stamped.transform.translation.x = tracker_pose.pos[0];
+        tracker->transform_stamped.transform.translation.y = tracker_pose.pos[1];
+        tracker->transform_stamped.transform.translation.z = tracker_pose.pos[2];
+
+        tracker->transform_stamped.transform.rotation.x = tracker_pose.quat[0];
+        tracker->transform_stamped.transform.rotation.y = tracker_pose.quat[1];
+        tracker->transform_stamped.transform.rotation.z = tracker_pose.quat[2];
+        tracker->transform_stamped.transform.rotation.w = tracker_pose.quat[3];
+
+        tf_broadcaster.sendTransform(tracker->transform_stamped);
+
+      }
+
+    }
+
+    static void VRPN_CALLBACK handle_twist(void *userData, const vrpn_TRACKERVELCB tracker_twist)
+    {
+      VrpnTrackerRos *tracker = (VrpnTrackerRos *) userData;
+      if (tracker->twist_pub.getTopic().empty())
+      {
+        tracker->twist_pub = tracker->nh_.advertise<geometry_msgs::TwistStamped>("twist", 1);
+      }
+
+      if (tracker->twist_pub.getNumSubscribers() > 0)
+      {
+
+        if (tracker->use_server_time_)
+        {
+          tracker->twist_msg.header.stamp.sec = tracker_twist.msg_time.tv_sec;
+          tracker->twist_msg.header.stamp.nsec = tracker_twist.msg_time.tv_usec * 1000;
+        }
+        else
+        {
+          tracker->twist_msg.header.stamp = ros::Time::now();
+        }
+
+        tracker->twist_msg.twist.linear.x = tracker_twist.vel[0];
+        tracker->twist_msg.twist.linear.y = tracker_twist.vel[1];
+        tracker->twist_msg.twist.linear.z = tracker_twist.vel[2];
+
+        double roll, pitch, yaw;
+        tf2::Matrix3x3 rot_mat(
+            tf2::Quaternion(tracker_twist.vel_quat[0], tracker_twist.vel_quat[1], tracker_twist.vel_quat[2],
+                            tracker_twist.vel_quat[3]));
+        rot_mat.getRPY(roll, pitch, yaw);
+
+        tracker->twist_msg.twist.angular.x = roll;
+        tracker->twist_msg.twist.angular.y = pitch;
+        tracker->twist_msg.twist.angular.z = yaw;
+
+        tracker->twist_pub.publish(tracker->twist_msg);
+      }
+    }
+
+    static void VRPN_CALLBACK handle_accel(void *userData, const vrpn_TRACKERACCCB tracker_accel)
+    {
+      VrpnTrackerRos *tracker = (VrpnTrackerRos *) userData;
+      if (tracker->accel_pub.getTopic().empty())
+      {
+        tracker->accel_pub = tracker->nh_.advertise<geometry_msgs::AccelStamped>("accel", 1);
+      }
+
+      if (tracker->accel_pub.getNumSubscribers() > 0)
+      {
+        if (tracker->use_server_time_)
+        {
+          tracker->accel_msg.header.stamp.sec = tracker_accel.msg_time.tv_sec;
+          tracker->accel_msg.header.stamp.nsec = tracker_accel.msg_time.tv_usec * 1000;
+        }
+        else
+        {
+          tracker->accel_msg.header.stamp = ros::Time::now();
+        }
+
+        tracker->accel_msg.accel.linear.x = tracker_accel.acc[0];
+        tracker->accel_msg.accel.linear.y = tracker_accel.acc[1];
+        tracker->accel_msg.accel.linear.z = tracker_accel.acc[2];
+
+        double roll, pitch, yaw;
+        tf2::Matrix3x3 rot_mat(
+            tf2::Quaternion(tracker_accel.acc_quat[0], tracker_accel.acc_quat[1], tracker_accel.acc_quat[2],
+                            tracker_accel.acc_quat[3]));
+        rot_mat.getRPY(roll, pitch, yaw);
+
+        tracker->accel_msg.accel.angular.x = roll;
+        tracker->accel_msg.accel.angular.y = pitch;
+        tracker->accel_msg.accel.angular.z = yaw;
+
+        tracker->accel_pub.publish(tracker->accel_msg);
+
+      }
+    }
+
+  };
+
+  class VrpnClientRos
   {
 
-  }
+  public:
 
-  return 0;
+
+    typedef std::map<std::string, VrpnTrackerRos> TrackerMap;
+
+
+    VrpnClientRos(ros::NodeHandle nh, ros::NodeHandle private_nh)
+    {
+
+      output_nh_ = private_nh;
+
+      private_nh.param<std::string>("frame_id", frame_id_, "world");
+      private_nh.param<bool>("use_server_time", use_server_time_, false);
+      private_nh.param<bool>("broadcast_tf", broadcast_tf_, false);
+
+      std::stringstream address_stream;
+      std::string server;
+      int port;
+
+      private_nh.param<std::string>("server", server, "localhost");
+      address_stream << server;
+
+      if (private_nh.getParam("port", port))
+      {
+        address_stream << ":" << port;
+      }
+      address_ = address_stream.str();
+
+      connection_ = boost::shared_ptr<vrpn_Connection>(vrpn_get_connection_by_name(address_.c_str()));
+
+      double update_frequency;
+      private_nh.param<double>("update_frequency", update_frequency, 100.0);
+      mainloop_timer = nh.createTimer(ros::Duration(1 / update_frequency), boost::bind(&VrpnClientRos::mainloop, this));
+
+      double refresh_tracker_frequency;
+      private_nh.param<double>("refresh_tracker_frequency", refresh_tracker_frequency, 0.0);
+
+      if (refresh_tracker_frequency > 0.0)
+      {
+        refresh_tracker_timer_ = nh.createTimer(ros::Duration(1 / refresh_tracker_frequency),
+                                                boost::bind(&VrpnClientRos::updateTrackers, this));
+      }
+
+      std::vector<std::string> param_tracker_names_;
+      if (nh.getParam("trackers", param_tracker_names_))
+      {
+        for (std::vector<std::string>::iterator it = param_tracker_names_.begin();
+             it != param_tracker_names_.end(); ++it)
+        {
+          trackers_.insert(std::make_pair(*it, VrpnTrackerRos(*it, connection_, output_nh_, frame_id_, use_server_time_,
+                                                              broadcast_tf_)));
+        }
+      }
+    }
+
+  private:
+
+    std::string address_, frame_id_;
+    TrackerMap trackers_;
+    ConnectionPtr connection_;
+    ros::NodeHandle output_nh_;
+    bool use_server_time_, broadcast_tf_;
+
+    ros::Timer refresh_tracker_timer_, mainloop_timer;
+
+    void mainloop()
+    {
+      connection_->mainloop();
+      for (TrackerMap::iterator it = trackers_.begin(); it != trackers_.end(); ++it)
+      {
+        it->second.mainloop();
+      }
+    }
+
+    void updateTrackers()
+    {
+
+      std::vector<std::string> tracker_names, sender_names, to_create, to_remove;
+
+      int i = 0;
+      while (connection_->sender_name(i) != NULL)
+      {
+        sender_names.push_back(std::string(connection_->sender_name(i++)));
+      }
+
+      for (TrackerMap::iterator it = trackers_.begin(); it != trackers_.end(); ++it)
+      {
+        tracker_names.push_back(it->first);
+      }
+
+      std::set_difference(sender_names.begin(), sender_names.end(), tracker_names.begin(), tracker_names.end(),
+                          std::inserter(to_create, to_create.begin()));
+      std::set_difference(tracker_names.begin(), tracker_names.end(), sender_names.begin(), sender_names.end(),
+                          std::inserter(to_remove, to_remove.begin()));
+
+      for (std::vector<std::string>::iterator it = to_create.begin(); it != to_create.end(); ++it)
+      {
+        trackers_.insert(std::make_pair(*it, VrpnTrackerRos(*it, connection_,
+                                                            output_nh_, frame_id_, use_server_time_, broadcast_tf_)));
+      }
+
+      for (std::vector<std::string>::iterator it = to_remove.begin(); it != to_remove.end(); ++it)
+      {
+        trackers_.erase(*it);
+      }
+
+    }
+
+
+  };
+
 }
-
 
 int main(int argc, char **argv)
 {
@@ -117,40 +318,9 @@ int main(int argc, char **argv)
 
   ros::NodeHandle nh, private_nh("~");
 
-//  VrpnRos(nh, private_nh);
+  vrpn_ros::VrpnClientRos vrpn(nh, private_nh);
 
-  connection_ = boost::shared_ptr<vrpn_Connection>(vrpn_get_connection_by_name("10.27.15.72"));
-
-//  int got_connection_type = ((connection_.get())->*(&ConnectionMessageIdExposer::message_type_is_registered))(vrpn_got_connection);
-//  connection_->register_handler(got_connection_type, got_connection_handler, NULL, vrpn_ANY_SENDER);
-
-//  int dropped_connection_type = ((connection_.get())->*(&ConnectionMessageIdExposer::message_type_is_registered))(vrpn_got_connection);
-//  connection_->register_handler(dropped_connection_type, dropped_connection_handler, NULL, vrpn_ANY_SENDER);
-
-  connection_->mainloop();
-
-  position_m_id = ((connection_.get())->*(&ConnectionExposer::message_type_is_registered))(
-      "vrpn_Tracker Pos_Quat");
-  velocity_m_id = ((connection_.get())->*(&ConnectionExposer::message_type_is_registered))(
-      "vrpn_Tracker Velocity");
-  accel_m_id = ((connection_.get())->*(&ConnectionExposer::message_type_is_registered))(
-      "vrpn_Tracker Acceleration");
-
-  connection_->register_handler(vrpn_ANY_TYPE, handle_all, NULL, vrpn_ANY_SENDER);
-
-  ros::Rate r(100);
-  while (ros::ok())
-  {
-    connection_->mainloop();
-
-//    for(int i = 0; i < 10; i ++)
-//    {
-//      ROS_INFO_STREAM(i << " " << connection->sender_name(i));
-//    }
-
-    r.sleep();
-  }
-
-//  ros::spin();
+  ros::spin();
   return 0;
 }
+
