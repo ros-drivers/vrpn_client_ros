24ba2cc4574a762cef6b2d439a0157c23dd0e576
Paul Bovbel <pbovbel@clearpathrobotics.com>
2015-06-15 10:26:46 -0400

    Break out separate nodes for client and tracker, and clean up build

--- include/vrpn_ros/vrpn_ros.h
+++ include/vrpn_ros/vrpn_ros.h
@@ -1,23 +1,49 @@
-//
-// Created by pbovbel on 12/06/15.
-//
+/**
+*
+*  \author     Paul Bovbel <pbovbel@clearpathrobotics.com>
+*  \copyright  Copyright (c) 2015, Clearpath Robotics, Inc.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*     * Redistributions of source code must retain the above copyright
+*       notice, this list of conditions and the following disclaimer.
+*     * Redistributions in binary form must reproduce the above copyright
+*       notice, this list of conditions and the following disclaimer in the
+*       documentation and/or other materials provided with the distribution.
+*     * Neither the name of Clearpath Robotics, Inc. nor the
+*       names of its contributors may be used to endorse or promote products
+*       derived from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+* ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL CLEARPATH ROBOTICS, INC. BE LIABLE FOR ANY
+* DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+* LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+* ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*
+* Please send comments, questions, or patches to code@clearpathrobotics.com
+*
+*/
 
-#ifndef PROJECT_VRPN_ROS_H
-#define PROJECT_VRPN_ROS_H
-
+#ifndef VRPN_ROS_VRPN_ROS_H
+#define VRPN_ROS_VRPN_ROS_H
 
 #include "vrpn_ros/vrpn_ros.h"
+
+#include "ros/ros.h"
+#include "geometry_msgs/PoseStamped.h"
+#include "geometry_msgs/TwistStamped.h"
+#include "geometry_msgs/AccelStamped.h"
+#include "geometry_msgs/TransformStamped.h"
+
 #include <vrpn_Tracker.h>
 #include <vrpn_Connection.h>
-#include <vrpn_Types.h>
-#include "ros/ros.h"
-#include <geometry_msgs/PoseStamped.h>
-#include <geometry_msgs/TwistStamped.h>
-#include <geometry_msgs/AccelStamped.h>
-#include <geometry_msgs/TransformStamped.h>
-#include <tf2/LinearMath/Quaternion.h>
-#include <tf2/LinearMath/Matrix3x3.h>
-#include <tf2_ros/transform_broadcaster.h>
+#include <map>
+#include <string>
 
 namespace vrpn_ros
 {
@@ -27,31 +53,33 @@ namespace vrpn_ros
 
   class VrpnTrackerRos
   {
-
   public:
-
     /**
      * Create and initialize VrpnTrackerRos using an existing underlying VRPN connection object.
      */
-    VrpnTrackerRos(std::string tracker_name, ConnectionPtr connection, ros::NodeHandle nh, std::string frame_id,
-                   bool use_server_time, bool broadcast_tf);
+    VrpnTrackerRos(std::string tracker_name, ConnectionPtr connection, ros::NodeHandle nh);
 
     /**
      * Create and initialize VrpnTrackerRos, creating a new connection to tracker_name@host.
      */
-    VrpnTrackerRos(std::string tracker_name, std::string host, ros::NodeHandle nh, std::string frame_id,
-                   bool use_server_time, bool broadcast_tf);
+    VrpnTrackerRos(std::string tracker_name, std::string host, ros::NodeHandle nh);
 
     ~VrpnTrackerRos();
 
-    void spin();
+    void init(std::string tracker_name, ros::NodeHandle nh, bool create_mainloop_timer);
+
+    /**
+     * Call mainloop of underlying vrpn_Tracker_Remote
+     */
+    void mainloop();
 
   private:
-
     TrackerRemotePtr tracker_remote_;
     ros::Publisher pose_pub, twist_pub, accel_pub;
-    ros::NodeHandle nh_;
+    ros::NodeHandle output_nh_;
     bool use_server_time_, broadcast_tf_;
+
+    ros::Timer mainloop_timer;
 
     geometry_msgs::PoseStamped pose_msg_;
     geometry_msgs::TwistStamped twist_msg_;
@@ -63,21 +91,29 @@ namespace vrpn_ros
     static void VRPN_CALLBACK handle_twist(void *userData, const vrpn_TRACKERVELCB tracker_twist);
 
     static void VRPN_CALLBACK handle_accel(void *userData, const vrpn_TRACKERACCCB tracker_accel);
-
   };
 
   class VrpnClientRos
   {
-
   public:
-
     /**
      * Create and initialize VrpnClientRos object in the private_nh namespace.
      */
     VrpnClientRos(ros::NodeHandle nh, ros::NodeHandle private_nh);
 
-  private:
+    static std::string getHostStringFromParams(ros::NodeHandle host_nh);
 
+    /**
+     * Call mainloop of underlying VRPN connection and all registered VrpnTrackerRemote objects.
+     */
+    void mainloop();
+
+    /**
+     * Examine vrpn_Connection and create or remove tracker objects as necessary
+     */
+    void updateTrackers();
+
+  private:
     std::string host_, frame_id_;
     bool use_server_time_, broadcast_tf_;
     ros::NodeHandle output_nh_;
@@ -93,17 +129,7 @@ namespace vrpn_ros
     std::map<std::string, VrpnTrackerRos> trackers_;
 
     ros::Timer refresh_tracker_timer_, mainloop_timer;
-
-    /**
-     * Spin mainloop of underlying VRPN connection and all registered VrpnTrackerRemote objects.
-     */
-    void spin();
-
-    /**
-     * Examine vrpn_Connection and create or remove tracker objects as necessary
-     */
-    void updateTrackers();
   };
-}
+}  // namespace vrpn_ros
 
-#endif //PROJECT_VRPN_ROS_H
+#endif  // VRPN_ROS_VRPN_ROS_H
--- src/vrpn_ros.cpp
+++ src/vrpn_ros.cpp
@@ -1,51 +1,84 @@
+/**
+*
+*  \author     Paul Bovbel <pbovbel@clearpathrobotics.com>
+*  \copyright  Copyright (c) 2015, Clearpath Robotics, Inc.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*     * Redistributions of source code must retain the above copyright
+*       notice, this list of conditions and the following disclaimer.
+*     * Redistributions in binary form must reproduce the above copyright
+*       notice, this list of conditions and the following disclaimer in the
+*       documentation and/or other materials provided with the distribution.
+*     * Neither the name of Clearpath Robotics, Inc. nor the
+*       names of its contributors may be used to endorse or promote products
+*       derived from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+* ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL CLEARPATH ROBOTICS, INC. BE LIABLE FOR ANY
+* DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+* LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+* ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*
+* Please send comments, questions, or patches to code@clearpathrobotics.com
+*
+*/
+
 #include "vrpn_ros/vrpn_ros.h"
-#include <vrpn_Tracker.h>
-#include <vrpn_Connection.h>
-#include <vrpn_Types.h>
-#include "ros/ros.h"
-#include <geometry_msgs/PoseStamped.h>
-#include <geometry_msgs/TwistStamped.h>
-#include <geometry_msgs/AccelStamped.h>
-#include <geometry_msgs/TransformStamped.h>
-#include <tf2/LinearMath/Quaternion.h>
-#include <tf2/LinearMath/Matrix3x3.h>
-#include <tf2_ros/transform_broadcaster.h>
+
+#include "tf2/LinearMath/Quaternion.h"
+#include "tf2/LinearMath/Matrix3x3.h"
+#include "tf2_ros/transform_broadcaster.h"
+
+#include <vector>
 
 namespace vrpn_ros
 {
 
-  VrpnTrackerRos::VrpnTrackerRos(std::string tracker_name, ConnectionPtr connection, ros::NodeHandle nh,
-                                 std::string frame_id,
-                                 bool use_server_time, bool broadcast_tf)
-      : nh_(nh, tracker_name), use_server_time_(use_server_time), broadcast_tf_(broadcast_tf)
+  VrpnTrackerRos::VrpnTrackerRos(std::string tracker_name, ConnectionPtr connection, ros::NodeHandle nh)
   {
-    ROS_INFO_STREAM("Creating tracker " << tracker_name);
-
-    pose_msg_.header.frame_id = twist_msg_.header.frame_id = accel_msg_.header.frame_id = transform_stamped_.header.frame_id = frame_id;
-    transform_stamped_.child_frame_id = tracker_name;
-
     tracker_remote_ = boost::make_shared<vrpn_Tracker_Remote>(tracker_name.c_str(), connection.get());
-    tracker_remote_->register_change_handler(this, &VrpnTrackerRos::handle_pose);
-    tracker_remote_->register_change_handler(this, &VrpnTrackerRos::handle_twist);
-    tracker_remote_->register_change_handler(this, &VrpnTrackerRos::handle_accel);
+    init(tracker_name, nh, false);
   }
 
-  VrpnTrackerRos::VrpnTrackerRos(std::string tracker_name, std::string host, ros::NodeHandle nh, std::string frame_id,
-                                 bool use_server_time, bool broadcast_tf)
-      : nh_(nh, tracker_name), use_server_time_(use_server_time), broadcast_tf_(broadcast_tf)
+  VrpnTrackerRos::VrpnTrackerRos(std::string tracker_name, std::string host, ros::NodeHandle nh)
+  {
+    std::string tracker_address;
+    tracker_address = tracker_name + "@" + host;
+    tracker_remote_ = boost::make_shared<vrpn_Tracker_Remote>(tracker_address.c_str());
+    init(tracker_name, nh, true);
+  }
+
+  void VrpnTrackerRos::init(std::string tracker_name, ros::NodeHandle nh, bool create_mainloop_timer)
   {
     ROS_INFO_STREAM("Creating tracker " << tracker_name);
+
+    output_nh_ = ros::NodeHandle(nh, tracker_name);
+
+    std::string frame_id;
+    nh.param<std::string>("frame_id", frame_id, "world");
+    nh.param<bool>("use_server_time", use_server_time_, false);
+    nh.param<bool>("broadcast_tf", broadcast_tf_, false);
 
     pose_msg_.header.frame_id = twist_msg_.header.frame_id = accel_msg_.header.frame_id = transform_stamped_.header.frame_id = frame_id;
     transform_stamped_.child_frame_id = tracker_name;
 
-    std::string tracker_address;
-    tracker_address = tracker_name + "@" + host;
-
-    tracker_remote_ = boost::make_shared<vrpn_Tracker_Remote>(tracker_address.c_str());
     tracker_remote_->register_change_handler(this, &VrpnTrackerRos::handle_pose);
     tracker_remote_->register_change_handler(this, &VrpnTrackerRos::handle_twist);
     tracker_remote_->register_change_handler(this, &VrpnTrackerRos::handle_accel);
+
+    if (create_mainloop_timer)
+    {
+      double update_frequency;
+      nh.param<double>("update_frequency", update_frequency, 100.0);
+      mainloop_timer = nh.createTimer(ros::Duration(1 / update_frequency),
+                                      boost::bind(&VrpnTrackerRos::mainloop, this));
+    }
   }
 
   VrpnTrackerRos::~VrpnTrackerRos()
@@ -55,18 +88,17 @@ namespace vrpn_ros
     tracker_remote_->unregister_change_handler(this, &VrpnTrackerRos::handle_accel);
   }
 
-  void VrpnTrackerRos::spin()
+  void VrpnTrackerRos::mainloop()
   {
     tracker_remote_->mainloop();
   }
 
   void VRPN_CALLBACK VrpnTrackerRos::handle_pose(void *userData, const vrpn_TRACKERCB tracker_pose)
   {
-
-    VrpnTrackerRos *tracker = (VrpnTrackerRos *) userData;
+    VrpnTrackerRos *tracker = static_cast<VrpnTrackerRos *>(userData);
     if (tracker->pose_pub.getTopic().empty())
     {
-      tracker->pose_pub = tracker->nh_.advertise<geometry_msgs::PoseStamped>("pose", 1);
+      tracker->pose_pub = tracker->output_nh_.advertise<geometry_msgs::PoseStamped>("pose", 1);
     }
 
     if (tracker->pose_pub.getNumSubscribers() > 0)
@@ -95,7 +127,6 @@ namespace vrpn_ros
 
     if (tracker->broadcast_tf_)
     {
-
       static tf2_ros::TransformBroadcaster tf_broadcaster;
       tracker->transform_stamped_.transform.translation.x = tracker_pose.pos[0];
       tracker->transform_stamped_.transform.translation.y = tracker_pose.pos[1];
@@ -107,22 +138,19 @@ namespace vrpn_ros
       tracker->transform_stamped_.transform.rotation.w = tracker_pose.quat[3];
 
       tf_broadcaster.sendTransform(tracker->transform_stamped_);
-
     }
-
   }
 
   void VRPN_CALLBACK VrpnTrackerRos::handle_twist(void *userData, const vrpn_TRACKERVELCB tracker_twist)
   {
-    VrpnTrackerRos *tracker = (VrpnTrackerRos *) userData;
+    VrpnTrackerRos *tracker = static_cast<VrpnTrackerRos *>(userData);
     if (tracker->twist_pub.getTopic().empty())
     {
-      tracker->twist_pub = tracker->nh_.advertise<geometry_msgs::TwistStamped>("twist", 1);
+      tracker->twist_pub = tracker->output_nh_.advertise<geometry_msgs::TwistStamped>("twist", 1);
     }
 
     if (tracker->twist_pub.getNumSubscribers() > 0)
     {
-
       if (tracker->use_server_time_)
       {
         tracker->twist_msg_.header.stamp.sec = tracker_twist.msg_time.tv_sec;
@@ -153,10 +181,10 @@ namespace vrpn_ros
 
   void VRPN_CALLBACK VrpnTrackerRos::handle_accel(void *userData, const vrpn_TRACKERACCCB tracker_accel)
   {
-    VrpnTrackerRos *tracker = (VrpnTrackerRos *) userData;
+    VrpnTrackerRos *tracker = static_cast<VrpnTrackerRos *>(userData);
     if (tracker->accel_pub.getTopic().empty())
     {
-      tracker->accel_pub = tracker->nh_.advertise<geometry_msgs::AccelStamped>("accel", 1);
+      tracker->accel_pub = tracker->output_nh_.advertise<geometry_msgs::AccelStamped>("accel", 1);
     }
 
     if (tracker->accel_pub.getNumSubscribers() > 0)
@@ -186,38 +214,25 @@ namespace vrpn_ros
       tracker->accel_msg_.accel.angular.z = yaw;
 
       tracker->accel_pub.publish(tracker->accel_msg_);
-
     }
   }
 
   VrpnClientRos::VrpnClientRos(ros::NodeHandle nh, ros::NodeHandle private_nh)
   {
-
     output_nh_ = private_nh;
 
     private_nh.param<std::string>("frame_id", frame_id_, "world");
     private_nh.param<bool>("use_server_time", use_server_time_, false);
     private_nh.param<bool>("broadcast_tf", broadcast_tf_, false);
 
-    std::stringstream host_stream;
-    std::string server;
-    int port;
+    host_ = getHostStringFromParams(private_nh);
 
-    private_nh.param<std::string>("server", server, "localhost");
-    host_stream << server;
-
-    if (private_nh.getParam("port", port))
-    {
-      host_stream << ":" << port;
-    }
-    host_ = host_stream.str();
-
-    ROS_INFO_STREAM("Connecting to VRPN server at " << host_stream.str());
+    ROS_INFO_STREAM("Connecting to VRPN server at " << host_);
     connection_ = boost::shared_ptr<vrpn_Connection>(vrpn_get_connection_by_name(host_.c_str()));
 
     double update_frequency;
     private_nh.param<double>("update_frequency", update_frequency, 100.0);
-    mainloop_timer = nh.createTimer(ros::Duration(1 / update_frequency), boost::bind(&VrpnClientRos::spin, this));
+    mainloop_timer = nh.createTimer(ros::Duration(1 / update_frequency), boost::bind(&VrpnClientRos::mainloop, this));
 
     double refresh_tracker_frequency;
     private_nh.param<double>("refresh_tracker_frequency", refresh_tracker_frequency, 0.0);
@@ -234,18 +249,33 @@ namespace vrpn_ros
       for (std::vector<std::string>::iterator it = param_tracker_names_.begin();
            it != param_tracker_names_.end(); ++it)
       {
-        trackers_.insert(std::make_pair(*it, VrpnTrackerRos(*it, connection_, output_nh_, frame_id_, use_server_time_,
-                                                            broadcast_tf_)));
+        trackers_.insert(std::make_pair(*it, VrpnTrackerRos(*it, connection_, output_nh_)));
       }
     }
   }
 
-  void VrpnClientRos::spin()
+  std::string VrpnClientRos::getHostStringFromParams(ros::NodeHandle host_nh)
+  {
+    std::stringstream host_stream;
+    std::string server;
+    int port;
+
+    host_nh.param<std::string>("server", server, "localhost");
+    host_stream << server;
+
+    if (host_nh.getParam("port", port))
+    {
+      host_stream << ":" << port;
+    }
+    return host_stream.str();
+  }
+
+  void VrpnClientRos::mainloop()
   {
     connection_->mainloop();
     for (std::map<std::string, VrpnTrackerRos>::iterator it = trackers_.begin(); it != trackers_.end(); ++it)
     {
-      it->second.spin();
+      it->second.mainloop();
     }
   }
 
@@ -271,8 +301,7 @@ namespace vrpn_ros
 
     for (std::vector<std::string>::iterator it = to_create.begin(); it != to_create.end(); ++it)
     {
-      trackers_.insert(std::make_pair(*it, VrpnTrackerRos(*it, connection_, output_nh_, frame_id_, use_server_time_,
-                                                          broadcast_tf_)));
+      trackers_.insert(std::make_pair(*it, VrpnTrackerRos(*it, connection_, output_nh_)));
     }
 
     for (std::vector<std::string>::iterator it = to_remove.begin(); it != to_remove.end(); ++it)
@@ -280,14 +309,4 @@ namespace vrpn_ros
       trackers_.erase(*it);
     }
   }
-}
-
-int main(int argc, char **argv)
-{
-  ros::init(argc, argv, "vrpn_ros_node");
-  ros::NodeHandle nh, private_nh("~");
-  vrpn_ros::VrpnClientRos vrpn(nh, private_nh);
-  ros::spin();
-  return 0;
-}
-
+}  // namespace vrpn_ros
